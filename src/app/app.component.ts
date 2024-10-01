8be49e8e7c19bc5631985e6b903b7337e95e5367
moisesalmeidapj <moises.albuquerque@precojusto.app.br>
2024-10-01 16:54:45 -0400

    Aplicando seletores especiais em new-component.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NewComponent],
+
   template:  `
   <!--  <router-outlet /> -->
     <h1>Curso de Angular</h1>
-    <h2>Components</h2>
-    
+    <div class="theme-dark">
+      <app-new-component></app-new-component>
+    </div>
+    <app-new-component>
   `
 })
 export class AppComponent {
